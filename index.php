bca0db747c52f38f835466797530a4aa1eb7a5f5
Jairo Beltrán <jbeltranjimenez@gmail.com>
2024-03-11 18:34:32 -0500

    se agregan estilos a login

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Usuarios</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <h1>Por favor registarte</h1>
+    <a href="registro.php">Registro</a>
+    <a href="index.php">Ingreso</a>
 </body>
 </html>
---
+++ registro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1 class="login">Login</h1>
+
+    <form class="formulario" action="registro.php" method="post"></form>
+    <input type="tex" name="Usuario" placeholder="Ingrese un usuario ">
+    <input type="password" name="Contraseña" placeholder="Ingrese su contraaseña">
+    <input type="confirmacion" name="confrimación" placeholder="Confirrme su contraseña">
+    <input type="Enviar" value="Enviar">
+   
+</body>
+
+</html>
