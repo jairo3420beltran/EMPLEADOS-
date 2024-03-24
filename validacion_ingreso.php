4a53a1fe7adffe2c590aab586139e92a44899175
Jairo Beltrán <jbeltranjimenez@gmail.com>
2024-03-23 22:20:22 -0500

    validación usuario

--- validacion_ingreso.php
+++ validacion_ingreso.php
@@ -1,33 +1,48 @@
 <?php
 // Verificar si se envió el formulario de inicio de sesión
-if (isset($_POST['ingreso'])) {
+// if (isset($_POST['ingreso'])) {
     // Conexión a la base de datos (asumiendo que tienes una conexión establecida)
-    include("database.php");
+    
 
     // Obtener los datos del formulario
     $usuario = $_POST['usuario'];
     $contraseña = $_POST['contraseña'];
+    $section_login= true;
 
+    include("database.php");
+    $conex = mysqli_connect('localhost','root','1234567890', 'bd_empleado',3306);
     // Consulta SQL para verificar las credenciales del usuario
-    $consulta = "SELECT * FROM registro WHERE usuario = '$usuario' AND contraseña = '$contraseña'";
-    $resultado = mysqli_query($conex, $consulta);
+    $query = "SELECT * FROM registro WHERE usuario = '$usuario' AND contraseña = '$contraseña'";
+    $resultado = mysqli_query($conex, $query);
 
-    // Verificar si se encontró un usuario con las credenciales proporcionadas
-    if (mysqli_num_rows($resultado) == 1) {
-        // Iniciar sesión
-        session_start();
-        $_SESSION['usuario'] = $usuario;
-
-        // Redireccionar al usuario a otra página
-        header("Location: otra_pagina.php");
-        exit; // Asegurar que el script se detenga después de la redirección
-    } else {
-        // Mostrar un mensaje de error si las credenciales son incorrectas
-        echo "Usuario o contraseña incorrectos.";
-    }
+    try{
+if (mysql_result( $resultado,0) ) 
+{
+    $result =mysql_result( $resultado,0);
+    echo"Usuario validado correctamente"
+}else{
+    echo"usuario o password erroneos"
 }
+}catch(Exception $e)
+mysql_close( $conex );
+
+
+    // // Verificar si se encontró un usuario con las credenciales proporcionadas
+    // if (mysqli_num_rows($resultado) == 1) {
+    //     // Iniciar sesión
+    //     session_start();
+    //     $_SESSION['usuario'] = $usuario;
+
+    //     // Redireccionar al usuario a otra página
+    //     header("Location: perfil.php");
+    //     exit; // Asegurar que el script se detenga después de la redirección
+    // } else {
+    //     // Mostrar un mensaje de error si las credenciales son incorrectas
+    //     echo "Usuario o contraseña incorrectos.";
+    // }
+// }
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -44,4 +59,4 @@ if (isset($_POST['ingreso'])) {
         <input type="submit" name="login" value="Iniciar sesión">
     </form>
 </body>
-</html>
+</html> -->
